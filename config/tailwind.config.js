fe5b87735d7fc94019dd459de2931f836910d5bd
ujjawalkr67 <ujjawalkr67@gmail.com>
2025-03-10 11:03:46 +0530

    email section

---
+++ config/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    content: [
+      "./app/views/**/*.html.erb",
+      "./app/helpers/**/*.rb",
+      "./app/assets/stylesheets/**/*.css",
+      "./app/javascript/**/*.js"
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          jakarta: ["'Plus Jakarta Sans'", "sans-serif"],
+        },
+      },
+    },
+    plugins: [],
+  };
+  
