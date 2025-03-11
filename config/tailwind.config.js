b4711ff6ab324f841f15ac8c736a2674c3a849ba
ujjawalkr67 <ujjawalkr67@gmail.com>
2025-03-11 22:56:11 +0530

    tailwind setup done

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,17 +1,16 @@
 module.exports = {
-    content: [
-      "./app/views/**/*.html.erb",
-      "./app/helpers/**/*.rb",
-      "./app/assets/stylesheets/**/*.css",
-      "./app/javascript/**/*.js"
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
-          jakarta: ["'Plus Jakarta Sans'", "sans-serif"],
-        },
+  content: [
+    "./app/views/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/assets/stylesheets/**/*.css",
+    "./app/javascript/**/*.js"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        jakarta: ["'Plus Jakarta Sans'", "sans-serif"],
       },
     },
-    plugins: [],
-  };
-  
+  },
+  plugins: [],
+};
